b72913b248158f69425856676c1b8c7d270714bb
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2020-12-29 22:17:18 +0100

    Added wip premake file.

---
+++ Make/TinyEngine.lua
@@ -0,0 +1,42 @@
+workspace "TinyEngine"
+   configurations { "Debug", "Release" }
+
+project "TinyEngine"
+   kind "ConsoleApp" 
+   language "C++"
+   targetdir "../bin/%{cfg.buildcfg}"
+   location "../Generated"
+
+   files { 
+      "../Tiny/**.hpp", 
+      "../Tiny/**.cpp" 
+   }
+
+   includedirs {
+      "../Tiny/**",
+      "../Tiny/Libs/"
+   }
+
+   sysincludedirs {
+      "../Tiny/Libs"
+   }
+
+   filter { "system:windows" }
+      links { "OpenGL32" }
+      
+   filter { "system:not windows" }
+      links { "Cocoa.framework", "CoreVideo.framework", "OpenGL.framework" }
+      files { 
+         "../Tiny/Platform/OSX/**.h", 
+         "../Tiny/Platform/OSX/**.mm", 
+      }
+   
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+      cppdialect "C++17"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+      cppdialect "C++17"
---
+++ Make/build_xcode.sh
@@ -0,0 +1,1 @@
+./Premake/premake5 --file=TinyEngine.lua xcode4	
