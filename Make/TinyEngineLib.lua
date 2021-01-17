540f06b2a42a5ab751b8832e684a0306bd1f372d
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2021-01-17 16:51:14 +0100

    Added lib premake scripts

---
+++ Make/TinyEngineLib.lua
@@ -0,0 +1,82 @@
+
+location "../Generated"
+
+workspace "TinyEngine"
+   configurations { "Debug", "Release" }
+
+project "TinyEngine"
+   kind "StaticLib" 
+   language "C++"
+   targetdir "../bin/%{cfg.buildcfg}"
+   
+
+   files { 
+      "../Tiny/**.hpp", 
+      "../Tiny/**.cpp",
+   }
+
+   includedirs {
+      "../Tiny/**",
+      "../Tiny/Libs/"
+   }
+
+   sysincludedirs {
+      "../Tiny/Libs"
+   }
+
+   filter { "system:windows" }
+      links { "OpenGL32" }
+      
+   filter { "system:not windows" }
+      links { "Cocoa.framework", "CoreVideo.framework", "OpenGL.framework" }
+      files { 
+         "../Tiny/Platform/OSX/**.h", 
+         "../Tiny/Platform/OSX/**.mm", 
+      }
+   
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+      cppdialect "C++17"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+      cppdialect "C++17"
+
+
+project "TestTinyEngine"
+   kind "WindowedApp" 
+   language "C++"
+   targetdir "../bin/%{cfg.buildcfg}"
+   
+
+   files { 
+      "../Examples/RotatingQuads.cpp"
+   }
+
+   includedirs {
+      "../Tiny/**"
+   }
+
+   sysincludedirs {
+      "../Tiny/Libs"
+   }
+
+   links { "TinyEngine" }
+
+   filter { "system:windows" }
+      links { "OpenGL32" }
+      
+   filter { "system:not windows" }
+      links { "Cocoa.framework", "CoreVideo.framework", "OpenGL.framework" }
+      
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+      cppdialect "C++17"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+      cppdialect "C++17"
---
+++ Make/build_xcode_lib.sh
@@ -0,0 +1,1 @@
+./Premake/premake5 --file=TinyEngineLib.lua xcode4	
