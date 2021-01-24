6ad0cd87fc1be61277db63cd325a15ec8e3a0b16
Jeppe Nielsen <nielsen_jeppe@hotmail.com>
2021-01-24 22:54:59 +0100

    Removed commented code in ScriptingEngine

--- Scripting/Source/ScriptingEngine.cpp
+++ Scripting/Source/ScriptingEngine.cpp
@@ -14,7 +14,6 @@
 
 #include "ScriptingEngine.hpp"
 
-
 using namespace Tiny;
 
 cling::Interpreter& GetInterpreter(void* interpreter) {
@@ -45,34 +44,6 @@ bool ScriptingEngine::Compile(const ScriptingContext &context) {
         argumentsList.push_back(s.c_str());
     }
     interpreter = new cling::Interpreter((int)argumentsList.size(), &argumentsList[0], clangLocation.c_str(), {}, true);
-    
-    /*
-    std::string sdkPath = pipe_to_string("xcrun --sdk macosx --show-sdk-path") + "/usr/include";
-    
-    std::vector<std::string> arguments;
-    
-    arguments.push_back("-I/Jeppes/TinyEngine/Scripting/Source/Scripts");
-    arguments.push_back("-I" + sdkPath);
-    arguments.push_back("-I/Users/jeppe/Downloads/cling_2020-11-05_mac1015/include");
-    
-    
-    //arguments.push_back("-I" + sdkPath);
-    //arguments.push_back("-I/Users/jeppe/Downloads/clang+llvm-10.0.0-x86_64-apple-darwin/lib/clang/10.0.0/include");
-    //arguments.push_back("-std=c++11");
-    //arguments.push_back("-stdlib=libstdc++");
-    //arguments.push_back("-I /Users/jeppe/Downloads/cling_2019-01-01_mac1013/lib/clang/5.0.0/include" );
-    //arguments.push_back("-I /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include" );
-    //arguments.push_back("-isystem /Users/jeppe/Downloads/clang+llvm-10.0.0-x86_64-apple-darwin/include/c++/v1/");
-    //arguments.push_back("-I /Users/jeppe/Downloads/clang+llvm-10.0.0-x86_64-apple-darwin/lib/clang/10.0.0/include");
-
-    
-    std::vector<const char*> argumentsList;
-    for(auto& s : arguments) {
-        argumentsList.push_back(s.c_str());
-    }
-
-    interpreter = new cling::Interpreter((int)argumentsList.size(), &argumentsList[0], "/Users/jeppe/Downloads/cling_2020-11-05_mac1015", {}, true);
-    */
     
     for(auto& cppFile : context.cppFiles) {
         CompileCppFile(cppFile);
@@ -110,18 +81,6 @@ void ScriptingEngine::CompileCppFile(const std::string &cppFile) {
     
     auto& interpreter = GetInterpreter(this->interpreter);
     interpreter.declare(code);
-     
-    /*
-    std::ifstream in(cppFile);
-    
-    std::string str;
-    std::string all;
-    while (std::getline(in, str)) {
-        all += str + "\n";
-    }
-    auto& interpreter = GetInterpreter(this->interpreter);
-    interpreter.declare(all);
-     */
 }
 
 std::vector<std::string> ScriptingEngine::CreateDefaultArguments() {
@@ -133,7 +92,6 @@ std::vector<std::string> ScriptingEngine::CreateDefaultArguments() {
     arguments.push_back("-v");
     arguments.push_back("-std=c++14");
     arguments.push_back("-fno-rtti");
-    //arguments.push_back("-I"+clangLocation+"/include");
     arguments.push_back("-I" + sdkPath);
     arguments.push_back("-I" + clangLocation + "/include/c++/v1");
     arguments.push_back("-I" + clangLocation + "/lib/clang/" + GetFirstFolder(clangLocation + "/lib/clang") + "/include");
